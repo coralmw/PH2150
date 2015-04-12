ff71518589a802e5d4125178a9364f29878e3a43
Thomas Parks <thomasparks@outlook.com>
2015-04-12 12:59:21 +0100

    Part 6 yay.
    Also ensured that thermo will not be added to repo

---
+++ pset1ex6.py
@@ -0,0 +1,8 @@
+thermo = open('thermodynamics.txt', 'w+')
+
+thermo.write('In all cases in which work is produced by the agency of heat, a quantity for heat is consumed which is proportional to the work done: and conversly, by the expenditure of an equal quantity of work and equal quantitiy for heat is produced')
+
+thermo.seek(0)
+print("The sixth word of this statment of the first law of thermodynamics is {}".format(thermo.read().split()[5]))
+thermo.seek(0)
+print(thermo.read())
