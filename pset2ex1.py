da830233051150f7fcd11a3b0d7ea49b1bf7192c
Thomas Parks <thomasparks@outlook.com>
2015-04-13 20:41:32 +0100

    getting further

--- pset2ex1.py
+++ pset2ex1.py
@@ -1,3 +1,13 @@
-#! /bin/bash
+P10 = ["hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", \
+       "nitrogen", "oxygen", "fluorine", "neon"]
+P20 = ["sodium", "magnesium", "aluminum", "silicon", "phosphorus", "sulfur", \
+       "chlorine", "argon", "potassium", "calcium"]
+P30 = ["scandium", "titanium", "vanadium", "chromium", "manganese", "iron", \
+       "cobalt", "nickel", "copper", "zinc"]
 
-sudo pip3 install periodictable
+Elems = P10 + P20
+for e in P30: #really
+    Elems.append(e)
+
+if __name__ == '__main__':
+    print(len(Elems))
