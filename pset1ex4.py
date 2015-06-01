eb8ace915bd8a0fa733ee5160520281ef9b31d1e
tomparks <thomasparks@outlook.com>
2015-06-01 14:07:57 +0100

    input mode

--- pset1ex4.py
+++ pset1ex4.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     try:
         # also handels non-int input
-        height, time = [float(n) for n in sys.argv[1:]]
+        height, time = [float(n) for n in input('Enter the height and time, space seperated: ').split()]
     except ValueError:
         print('Not a number. Usage: pset1ex4.py HEIGHT TIME')
         sys.exit()
