04202ee76512e479db82fe6a2010d0b7501c2d1c
Thomas Parks <thomasparks@outlook.com>
2015-11-04 23:51:33 +0000

    It kinda works, euler and RK are diffrent.

--- pset7ex2.py
+++ pset7ex2.py
@@ -7,21 +7,6 @@ magnetic fields. You can edit the program to change the components of the fields
 the trajectory.
 """
 
-class Electron(object):
-
-    def __init__(self, P, V, m, q):
-        self.position = P
-        self.velocity = V
-        self.mass = m
-        self.charge = q
-
-    def update(self, P, V):
-        self.position += P
-        self.velocity += V
-
-    def statecopy(self):
-        return self.position.copy(), self.velocity.copy()
-
 # nupmy gives inaccurate results unless 100x the timepoints taken
 # float64 no change
 # 0 -> 0. no change
@@ -39,17 +24,18 @@ dt = 0.0001
 Peuler = []
 E, B, P, V = [vec.copy() for vec in (Ei, Bi, Pi, Vi)]
 
-particle = Electron(P, V, m, q)
-
-def Iterate(particle, E=E, B=E, dt=dt):
-    F = particle.charge * (E + np.cross(particle.velocity, B))
-    dv = F/particle.mass * dt # Acceleration = F/m; dv = a.dt
-    dp = (V+dv)*dt
-    return dp, dv
+def ForceAtVelocity(V, E=E, B=B, q=q):
+    return q * (E + np.cross(V, B))
 
 while t < T: # trace path until time reaches value e.g. 10
-    particle.update( *Iterate(particle) )
-    Peuler.append(particle.statecopy())
+    f = ForceAtVelocity(V)
+    a = f / m
+    dv = a * dt
+    dp = (V+dv)*dt
+
+    V += dv
+    P += dp
+    Peuler.append(P.copy())
     t += dt
 
 Prk = []
@@ -57,18 +43,30 @@ E, B, P, V = [vec.copy() for vec in (Ei, Bi, Pi, Vi)]
 t = 0.
 
 while t < T: # trace path until time reaches value e.g. 10
-    firstorderUpdateParticle = Electron(*[start+update for start, update in
-                                        zip(particle.statecopy(), Iterate(*particle))]
-                                        particle.mass,
-                                        particle.charge)
-                                        
-    leftSecondUpdateParticle = Electron(*[start+update for start, update in
-                                        zip(particle.statecopy(), Iterate(particle))]
-                                        particle.mass,
-                                        particle.charge)
-    k3 = dP(P + k2/2, V)
-    k4 = dP(P + k3/2, V)
-    P = P + k1/6. + k2/3. + k3/3. + k4/6.
+    # first find the first-order update
+    f1 = ForceAtVelocity(V)
+    a1 = f1 / m
+    dv1 = a1 * dt
+    dp1 = (V+dv1) * dt # just using this gives the Euler method
+
+    # find the force at the first midpoint.
+    f2 = ForceAtVelocity(V+dv1)
+    a2 = f2 / m
+    dv2 = a2 * (dt/2.)
+    dp2 = (V+dv2) * dt/2.
+
+    f3 = ForceAtVelocity(V+dv2)
+    a3 = f3 / m
+    dv3 = a3 * (dt/2.)
+    dp3 = (V+dv3) * dt/2.
+
+    f4 = ForceAtVelocity(V+dv3)
+    a4 = f4 / m
+    dv4 = a4 * dt
+    dp4 = (V+dv4) * dt
+
+    V += dv1/6. + dv2/3. + dv3/3. + dv4/6.
+    P += dp1/6. + dp2/3. + dp3/3. + dp4/6.
     Prk.append(P.copy())
     t += dt
 
